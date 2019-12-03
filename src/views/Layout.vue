5b57ac3eb0df6a4db23c2bd8c28492f0ea1fde5d
tianjinwanghongwei <912117224@qq.com>
2019-12-03 21:25:53 +0800

    项目初始化

---
+++ src/views/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <!-- 导航栏 -->
+        <!-- 内容容器 -->
+        <!-- tab栏 -->
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Layout'
+}
+</script>
+
+<style lang="less" scoped>
+</style>
